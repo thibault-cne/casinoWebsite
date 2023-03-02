0af37ed9421a6a491b2b18d8059cb211cd1b791e
thibault-cne <thibault.chene23@gmail.com>
2023-03-02 14:48:15 +0100

    [~] try fix for pictures

--- backend/api/v1/handlers/user/get/picture.go
+++ backend/api/v1/handlers/user/get/picture.go
@@ -2,6 +2,7 @@ package get
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -9,10 +10,17 @@ import (
 
 func getPicture(ctx *gin.Context) {
 	userId := ctx.Param("userId")
-	filePath := fmt.Sprintf("./images/webp/%s.webp", userId)
+	path, err := os.Getwd()
+
+	if err != nil {
+		ctx.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+
+	filePath := fmt.Sprintf("%s/images/webp/%s.webp", path, userId)
 
 	if _, err := os.Stat(filePath); err != nil {
-		filePath = "./images/webp/default.webp"
+		filePath = fmt.Sprintf("%s/images/webp/default.webp", path)
 	}
 
 	ctx.File(filePath)
