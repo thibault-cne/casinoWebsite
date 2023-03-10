f8ea6c1043ef39ca39694991d9caf0c635d50e88
thibault-cne <thibault.chene23@gmail.com>
2023-03-10 21:45:15 +0100

    [+] fix spelling in navbar

--- frontend/src/components/navBar.vue
+++ frontend/src/components/navBar.vue
@@ -48,7 +48,7 @@
                     <router-link
                       to="/admin/user"
                       class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                      >Dashboard utilisteurs</router-link
+                      >Dashboard utilisateurs</router-link
                     >
                   </li>
                   <li>
