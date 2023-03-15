5efe53a9a9610d927c18e1e034ca5987acf3c0d8
thibault-cne <thibault.chene23@gmail.com>
2023-03-15 21:23:50 +0100

    [+] add debugging prints

--- backend/api/v1/handlers/auth/login.go
+++ backend/api/v1/handlers/auth/login.go
@@ -39,7 +39,7 @@ func Login(c *gin.Context) {
 		return
 	}
 	fmt.Println("Error 2")
-	fmt.Printf("%s %+v\n", f.Password, user)
+	fmt.Printf("%s %s %+v\n", f.Username, f.Password, user)
 
 	// Check if the password is correct
 	if !user.CheckPasswordHash(f.Password) {
